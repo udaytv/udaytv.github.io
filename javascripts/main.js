8c9af4c9e409a90d96ff9ea33bb2d18277d68cb5
Dhval <mail@mail.com>
2014-03-22 16:02:06 -0400

    jojo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,20 @@
-function getStream(channel) {
-	var resp = $.post("/sc2/stream2vlc.php", {
-		channelname: channel,
-		hoster: "justin"
+var UTIL = (function($) {
+	var util = {}, stream2vlc = "/sc2/stream2vlc.php";
+	$("a.jlink").each(function(index, a) {
+		var id = a.id;
+		$.post(stream2vlc, {
+			channelname: id,
+			hoster: "justin"
+		}).done(function(data) {
+			console.log(data);
+			if (data && data.responseText && data.responseText.length > 250) {
+				var respTxt = resp.responseText.substr(17);
+				respTxt = respTxt.substr(0, respTxt.length - 2);
+				$("a#" + id).eq("0").attr({
+					"href": respTxt
+				})
+			}
+		});
+
 	});
-	if (resp && resp.responseText && resp.responseText.length > 250) {
-		var respTxt = resp.responseText.substr(17);
-		return respTxt.substr(0, respTxt.length - 2);
-	}
-}
+}(jQuery));
