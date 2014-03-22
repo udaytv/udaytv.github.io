8dd9fe5c7b3d61f5bd213f041042fbeee2bdc446
Dhval <mail@mail.com>
2014-03-22 13:41:57 -0400

    add f to get stream url

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+function getStream(channel) {
+	var resp = $.post("/sc2/stream2vlc.php", {
+		channelname: channel,
+		hoster: "justin"
+	});
+	if (resp && resp.responseText && resp.responseText.length > 250) {
+		var respTxt = resp.responseText.substr(17);
+		return respTxt.substr(0, respTxt.length - 2);
+	}
+}
